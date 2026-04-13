38fc393434c71a3bdd6279acaf3375432ac5fce2
rain <mu4317935@gmail.com>
2026-04-13 13:15:40 +0800

    放弃websocket方式，选择rabbitmq

--- crawlernode/src/main/java/com/example/crawlernode/CrawlernodeApplication.java
+++ crawlernode/src/main/java/com/example/crawlernode/CrawlernodeApplication.java
@@ -3,22 +3,11 @@ package com.example.crawlernode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.crawlernode.entity.task.SubTask;
-import com.example.crawlernode.service.TaskExecutor;
 @SpringBootApplication
-public class CrawlernodeApplication implements org.springframework.boot.CommandLineRunner {
+public class CrawlernodeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CrawlernodeApplication.class, args);
-	}
+        public static void main(String[] args) {
+                SpringApplication.run(CrawlernodeApplication.class, args);
+        }
 
-	@Override
-	public void run(String... args) {
-        SubTask task = new SubTask(
-                "https://baike.baidu.com/item/",
-                "文学"
-        );
-
-        TaskExecutor.execute(task);
-    }
 }
